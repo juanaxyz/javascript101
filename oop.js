ff3c0b10a791acd2d3589cd84f92000141610c95
juanaxyz <inijuana@gmail.com>
2026-01-12 09:47:27 +0800

    add constructor

--- oop.js
+++ oop.js
@@ -39,3 +39,37 @@ const circle = {
         
     }
 }
+
+
+// factory functions
+function createSquare(side){
+    return  {
+        side,
+        draw : function(){
+            console.log('sisi kotak ini adalah : ' + side)
+
+        },
+        hitungLuas : function(){
+            console.log('luas = ' + side*side);
+            
+        }
+    };
+}
+
+// const kotak1 = createSquare(10);
+// kotak1.draw();
+// kotak1.hitungLuas()
+
+
+// constractor
+function mahasiswa(NIM, nama){
+    this.NIM = NIM,
+    this.nama = nama,
+    this.getMhsInfo = function(){
+        console.log('Nama :', nama);
+        console.log('NIM :', NIM);
+    }
+}
+
+const juana = new mahasiswa(2405551091, "i wayan juana satya adinata");
+juana.getMhsInfo();
