489de5ec4fbe4e79ed712c9189c399a5ae561668
juanaxyz <inijuana@gmail.com>
2026-01-13 16:35:31 +0800

    implementasi javascript function component

---
+++ react/learn1.1/src/header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header>
+      <h1>my website</h1>
+      <h2>wellcome </h2>
+
+      <nav>
+        <ul class="list-disc list-inside">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Service</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
